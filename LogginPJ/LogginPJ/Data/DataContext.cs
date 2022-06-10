0f40d082111659e1e1b3918a8c57d9cc6455a632
Gabriel Leme <gabriel.leme.1@hotmail.com>
2022-06-09 17:15:30 -0700

    Initial Migration

---
+++ LogginPJ/LogginPJ/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using LogginPJ.Extensions;
+
+namespace LogginPJ.Data;
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var config = new ConfigurationBuilder()
+            .AddConfigurationFile(loadBaseFile: false, optional: false)
+            .Build();
+
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        }
+
+    }
+
+    public DbSet<User> Users => Set<User>();
+}
+
---
+++ LogginPJ/LogginPJ/Extensions/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace LogginPJ.Extensions;
+public static class ConfigurationBuilderExtensions
+{
+    public static IConfigurationBuilder AddConfigurationFile(this IConfigurationBuilder source,
+        string configFileName = "appsettings",
+        bool loadBaseFile = true,
+        bool optional = true,
+        string customDirectory = default)
+    {
+        var enviroment = System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        var result = source.SetBasePath(customDirectory ?? Directory.GetCurrentDirectory());
+
+        if (loadBaseFile)
+            result = result.AddJsonFile($"{configFileName}.json", optional: optional, reloadOnChange: true);
+
+        return result.AddJsonFile($"{configFileName}.{enviroment}.json", optional: optional, reloadOnChange: true);
+    }
+}
+
